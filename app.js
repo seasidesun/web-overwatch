0a28f2831e76ab4ca1d24c9055c453f31d5ec402
haibin <sunhaibin@goyoo.com>
2016-05-18 18:15:29 +0800

    add top pic

--- app.js
+++ app.js
@@ -25,9 +25,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('.hbs', hbs.engine);
 app.set('view engine', '.hbs');
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(__dirname + '/favicon.ico'));
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
